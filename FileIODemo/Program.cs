7cf7ffc1c888404cb8e52051124c7a5ba4ccb3d3
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-02-03 21:55:04 +0530

    Added InputOutput Demo File

---
+++ FileIODemo/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace FileIODemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Diaplaying File Stream Reader Program");
+            StreamReaderWriter readerWriter = new StreamReaderWriter();
+            readerWriter.CheckFileExist();
+            readerWriter.ReadFile();
+            readerWriter.WriteFile();
+        }
+    }
+}
---
+++ FileIODemo/StreamReaderWriter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIODemo
+{
+    public  class StreamReaderWriter
+    {
+        public string path;
+        public StreamReaderWriter()
+        {
+            path = @"C:\Users\sai\source\repos\RFP-244\FileIODemo\FileIODemo\DataText.txt";
+        }
+        public void CheckFileExist()
+        {
+            if(!File.Exists(path))
+            {
+                return;
+            }
+        }
+        public void ReadFile()
+        {
+            CheckFileExist();
+            using(StreamReader reader = File.OpenText(path))
+            {
+                string line = string.Empty;
+                while((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            Console.ReadKey();
+        }
+        public void WriteFile()
+        {
+            CheckFileExist();
+            using (StreamWriter writer = File.AppendText(path))
+            {
+                writer.WriteLine("\n Class is at eight o' clock \n Please be available \n Will have fun!!!");
+                writer.Close();
+            }
+            Console.ReadKey();
+        }
+    }
+}
